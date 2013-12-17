75de766a3454676a456a63919a0bd9f1cef5f0e6
kimberley-p <kim.pryor@hotmail.com>
2013-12-17 07:58:05 +0000

    HTTP SERVER exercise complete

---
+++ stream-adventure/7.js
@@ -0,0 +1,27 @@
+// Stream-adventure Exercise 6 HTTP SERVER
+
+var http = require('http');
+var fs = require('fs');
+var through = require('through');
+
+var port = process.argv[2];
+
+function onRequest(request, response) {
+	if(request.method ==='POST') {
+		request.pipe(tr).pipe(response);	
+	}
+}
+
+function write(data) { 
+	this.queue(data.toString().toUpperCase()) 
+  }
+  
+ function end () { 
+    this.queue(null)
+  }
+  
+var tr = through(write, end);
+
+
+http.createServer(onRequest).listen(port);
+
