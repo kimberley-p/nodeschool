b223879e5cd1e688870dbd8220d738935e365ed2
kimberley-p <kim.pryor@hotmail.com>
2013-12-20 13:59:01 +0000

    including instructions in each exercise

--- bytewiser/1.js
+++ bytewiser/1.js
@@ -1,4 +1,14 @@
 // nodeschool.io BYTEWISER - exercise 1 Buffer From String
 
-var thing  = new Buffer("bytewiser");
-console.log(thing);
+//INSTRUCTIONS
+// In Node.js, `Buffer` is a global object that provides a way to work with differe
+// nt
+// kinds of binary data. Most APIs in Node core (e.g. http, net, fs) will give you
+// buffers, and will be able to do their job faster if you give them buffers.
+
+// For this challenge, write a node program that prints a buffer object containing
+// the string "bytewiser" using console.log.
+
+
+var buf  = new Buffer("bytewiser");
+console.log(buf);
