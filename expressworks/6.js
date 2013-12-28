032e186d820ea2cb35a65cd5e576d970f43ca1d4
kimberley-p <kim.pryor@hotmail.com>
2013-12-28 18:02:13 +0000

    exercise 6

---
+++ expressworks/6.js
@@ -0,0 +1,26 @@
+// nodeschool.io EXPRESSWORKS - exercise 6 Param Pam Pam
+
+//INSTRUCTIONS
+
+
+// Create an Express.js server that processes PUT '/message/:id' requests, 
+// e.g., PUT '/message/526aa677a8ceb64569c9d4fb'
+
+// As the response of this request return id SHA1 hashed with a date:
+
+//   require('crypto')
+//     .createHash('sha1')
+//     .update(new Date().toDateString().toString() + id)
+//     .digest('hex')
+var express = require('express')
+var crypto = require('crypto');
+
+var app = express();
+
+app.put('/message/:id', function(req, res) {
+	var id = req.params.id;
+    var hashed = crypto.createHash('sha1').update(new Date().toDateString().toString() + id).digest('hex');
+    res.send(hashed);
+});
+
+app.listen(process.argv[2])
